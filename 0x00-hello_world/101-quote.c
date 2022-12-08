045225a820cc93e6c14bd8128eec6b182f2aa461
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2022-12-08 05:47:06 -0500

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <unistd.h>
 
 /**
@@ -10,6 +9,3 @@ int main(void)
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-=======
-
->>>>>>> 7a8af53dc3e2f88e34003890a3f763ee0c7fb35f
