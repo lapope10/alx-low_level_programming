7409b76b5fa63031e4f3b502fcc41b503ce198ec
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2022-12-08 04:48:03 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -12,6 +10,3 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-=======
-
->>>>>>> ed6c031fd587a4c1eb86fb7937242ea8c5f1ee1e
