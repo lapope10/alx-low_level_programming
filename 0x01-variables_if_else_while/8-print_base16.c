69ead335ee8d5fde06859455f1f0d41aabb2bcbb
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2022-12-09 11:40:27 -0500

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
- *
  * main - Print the numbers of base 16 in lowercase
  *
  * Return: Always 0 (Success)
@@ -15,13 +13,9 @@ int main(void)
 	putchar(digit);
 
 	for (digit = 'a'; digit <= 'f'; digit++)
-	
 	putchar(digit);
 
 	putchar('\n');
-
+	
 	return (0);
 }
-=======
-
->>>>>>> 8ce3174566319fba10374ceaa53e8366b9b6c758
