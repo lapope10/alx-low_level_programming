1b3278e0474f2dc0c791550943f1a2a0d9b90e0e
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2022-12-15 18:48:48 -0500

    Update 0-main.c

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <stdio.h>
 
@@ -17,6 +16,3 @@ int main(void)
 	printf("%c: %d\n", c, _isupper(c));
 	return (0);
 }
-=======
-
->>>>>>> 22fb0a9aad7d51e83a429fe2b3d782fef26d29fa
