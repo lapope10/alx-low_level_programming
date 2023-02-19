7ec52281bb3e94211f19d00233a11538dee56428
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2023-02-19 14:35:26 -0500

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -7,6 +6,3 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 #endif
-=======
-
->>>>>>> bd484736a8d6904040d435ae78bb9a53d8615972
